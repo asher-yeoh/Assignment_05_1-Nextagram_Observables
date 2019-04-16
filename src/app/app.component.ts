64ebb39758272509a10db72864c363ed5255e15e
ASHER <asher.yeoh@dell.com>
2019-04-16 14:36:25 +0800

    version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserListService } from './user-list.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Nextagram';
+
+  users: User[] = []
+
+  constructor(private userListService: UserListService){}
+
+  ngOnInit(){
+    this.userListService.getUsers().subscribe(users => {
+      this.users = users
+    })
+  }
+
+    // ngOnInit(){
+    //     console.log(this.userListService.users)
+    // }
+
+  // users: User[] = [
+  //   {
+  //     "id": 3, 
+  //     "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/idol2-blake.jpg", 
+  //     "username": "blake"
+  //   }, 
+  //   {
+  //     "id": 2, 
+  //     "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/idol1-ryan.jpg", 
+  //     "username": "ryanG"
+  //   }, 
+  //   {
+  //     "id": 283, 
+  //     "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/profile-placeholder.jpg", 
+  //     "username": "robertalan"
+  //   }, 
+  //   {
+  //     "id": 1, 
+  //     "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/bigfan-9AE7468E-4C35-41D1-AA68-31939891B5E1.png", 
+  //     "username": "bigfan"
+  //   }, 
+  //   {
+  //     "id": 307, 
+  //     "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/profile-placeholder.jpg", 
+  //     "username": "SoulMUSZE"
+  //   }, 
+  //   {
+  //     "id": 284, 
+  //     "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/scenery-IMG_0319-sm.jpg", 
+  //     "username": "testing123"
+  //   }, 
+  //   {
+  //     "id": 285, 
+  //     "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/profile-placeholder.jpg", 
+  //     "username": "hello"
+  //   }, 
+  //   {
+  //     "id": 6, 
+  //     "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/josht-avatar.png", 
+  //     "username": "josht"
+  //   }, 
+  // ]
+  
 }
---
+++ src/app/user.ts
@@ -0,0 +1,6 @@
+export interface User {
+    id: number;
+    profileImage: string;
+    username: string;
+    
+}
